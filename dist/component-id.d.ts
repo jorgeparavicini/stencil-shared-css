5a18af87758e5bb0bdba85333e0a6f38a3a92dbe
Jorge Paravicini <j.m.paravicini@gmail.com>
2021-06-11 15:19:52 +0200

    create component id

---
+++ dist/component-id.d.ts
@@ -0,0 +1,1 @@
+export default function componentId(): (target: Object, propertyKey: string) => void;
---
+++ src/component-id.ts
@@ -0,0 +1,3 @@
+export default function componentId() {
+  return function (target: Object, propertyKey: string) {};
+}
