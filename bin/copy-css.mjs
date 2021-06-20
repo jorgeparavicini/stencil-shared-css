b8633845cb7dc65ab023f3515b3d8c4629c70c79
Jorge Paravicini <j.m.paravicini@gmail.com>
2021-06-20 11:02:33 +0200

    copy-css v1

--- bin/copy-css.mjs
+++ bin/copy-css.mjs
@@ -10,6 +10,7 @@ import { v4 as uuid4 } from 'uuid';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const rootDir = __dirname.split('node_modules')[0];
 const assetsDirectory = rootDir + '/assets/';
+const componentDir = rootDir + '/dist/esm/';
 
 function readCss(fileContent) {
   // Matches everything after the string 'Css = "'
@@ -21,33 +22,40 @@ function readCss(fileContent) {
   return matches[1];
 }
 
-function writeCss(css, result) {
+function removeCssConstant(fileContent) {
+  const removeRegex = /[\n\r].*Css\s+=\s+\"([^\n\r]*)/;
+  return fileContent.replace(removeRegex, '');
+}
+
+function writeCss(css) {
   const filename = `${uuid4()}.css`;
-  fs.writeFile(assetsDirectory + filename, css, err => {
-    if (err) {
-      console.error(err);
-    }
-    result(filename);
-  });
+  fs.writeFileSync(assetsDirectory + filename, css);
+  return filename;
+}
+
+function replaceStyleProperty(fileContent, newCssFilename) {
+  const newProperty = `.__cssFilename = "${newCssFilename}"`;
+  return fileContent.replace(/.style\s*=\s*([^\n\r]*)/, newProperty);
 }
 
 if (!fs.existsSync(assetsDirectory)) {
   fs.mkdirSync(assetsDirectory);
 }
 
-glob(rootDir + '/dist/esm/*.entry.js', (err, files) => {
+glob(componentDir + '*.entry.js', (err, files) => {
   if (err != null) {
     console.error(err);
     return;
   }
 
   files.forEach(file => {
-    const css = readCss(fs.readFileSync(file));
-    console.log(css);
+    let fileContent = fs.readFileSync(file).toString('utf-8');
+    const css = readCss(fileContent);
     if (css == null) return;
 
-    writeCss(css, filename => {
-      console.log(filename);
-    })
+    fileContent = removeCssConstant(fileContent);
+    const filename = writeCss(css);
+    fileContent = replaceStyleProperty(fileContent, filename);
+    fs.writeFileSync(file, fileContent);
   });
 });
